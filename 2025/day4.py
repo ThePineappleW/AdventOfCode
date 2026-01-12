8d872ba315a62f2e35984f947e8127395ceed661
Emery <emery.jacobowitz@gmail.com>
2026-01-12 09:59:16 -0500

    Revised 2025 Day 4

--- 2025/day4.py
+++ 2025/day4.py
@@ -1,9 +1,6 @@
-#! /usr/bin/python3
+#! /usr/bin/env python
 
-from utils import Window
-
-from pprint import pprint
-from io import StringIO
+from utils import AOCrunner, Posn2D, Window
 
 INPUTFILE = r"inputs/day4.txt"
 
@@ -18,53 +15,65 @@ TESTCASE = """..@@.@@@@.
 .@@@@@@@@.
 @.@.@@@.@."""
 
+type Matrix = list[list[str]]
 
 
-def parse(f):
-  """Conver the input into a 2d matrix."""
-  arr = []
-  for line in f:
-    arr.append([])
-    for symbol in line.strip():
-      arr[-1].append(symbol)
-  return arr
+def parse(f) -> Matrix:
+    """
+    Convert the input into a 2d matrix.
+    Replace rolls with 1, and other slots with 0,
+    so that we can use math to count rolls.
+    """
+    mat = []
+    for line in f:
+        mat.append([])
+        for symbol in line.strip():
+            mat[-1].append(1 if symbol == "@" else 0)
+    return mat
 
-def count_accessible(arr: list[list[str]]) -> int:
-  rolls = []
-  count = 0
-  for i, window in enumerate(Window.slide_centered(arr, (3, 3))):
-    row, col = divmod(i, len(arr[0]))
-    if arr[row][col] == '@':
-      if sum(row.count("@") for row in window) < 5:
-        count += 1
-        rolls.append((row, col))
-        
-  return count, rolls
 
-def remove_rolls(arr: list[list[str]]) -> int:
-  accessible, rolls = count_accessible(arr)
-  if accessible == 0:
-    return 0
-  
-  for row, col in rolls:
-    arr[row][col] = '.'
-  return accessible + remove_rolls(arr)
-  
+def find_accessible(mat: Matrix) -> list[Posn2D]:
+    """
+    Compute a sliding window over the matrix, centered on each slot.
+    Because the shape of the window will change around the edges of the grid,
+    we need the `divmod(i, len(mat[0]))` to find the center.
 
-def solve1(f) -> int:
-  return count_accessible(parse(f))[0]
+    For part 1 we just need to count the rolls,
+    but for part 2 we need to know where they are.
+    Thus, we'll return a list of the coordinates of each roll.
+    """
+    rolls = []
+    for i, window in enumerate(Window.slide_centered(mat, (3, 3))):
+        row, col = divmod(i, len(mat[0]))
+        if mat[row][col]:
+            if sum(sum(row) for row in window) < 5:
+                rolls.append(Posn2D(row, col))
 
-def solve2(f) -> int:
-  return remove_rolls(parse(f))
+    return rolls
+
+
+def remove_rolls(mat: Matrix) -> int:
+    """
+    Recursively count the accessible rolls and remove them.
+    """
+    rolls = find_accessible(mat)
+    if not rolls:
+        return 0
+
+    for pos in rolls:
+        mat[pos.x][pos.y] = 0
+    return len(rolls) + remove_rolls(mat)
+
+
+def count_accessible_rolls(f) -> int:
+    return len(find_accessible(parse(f)))
+
+
+def count_removed_rolls(f) -> int:
+    return remove_rolls(parse(f))
 
 
 if __name__ == "__main__":
-  
-  assert solve1(StringIO(TESTCASE)) == 13
-  with open(INPUTFILE, "r") as f:
-    print("Part 1:", solve1(f))
-
-
-  assert solve2(StringIO(TESTCASE)) == 43
-  with open(INPUTFILE, "r") as f:
-    print("Part 2:", solve2(f))
+    aoc = AOCrunner(INPUTFILE, TESTCASE)
+    aoc.part1(count_accessible_rolls, 13)
+    aoc.part2(count_removed_rolls, 43)
